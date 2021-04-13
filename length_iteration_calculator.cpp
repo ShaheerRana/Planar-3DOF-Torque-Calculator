bbc9f5efd3512b62b1163c4a07712c9dc0be9132
Ethan Kim <robhn38426@gmail.com>
2021-04-13 13:01:44 -0700

    Applied vertified torque equation

--- length_iteration_calculator.cpp
+++ length_iteration_calculator.cpp
@@ -6,26 +6,26 @@ using namespace std;
 
 // Search Parameters:
 const double MAX_LENGTH = 1.0;
+const double MIN_LENGTH = 0.3;
 const double INCREMENT_LENGTH = 0.1;
-const double START_LENGTH = 0.2;
 
 // Position 1:
-const int q3_1 = -M_PI/3;
+const double q3_1 = -M_PI/3;
 const double cx1 = 0.75;
 const double cy1 = 0.1;
 // Position 2:
-const int q3_2 = 0;
+const double q3_2 = 0;
 const double cx2 = 0.5;
 const double cy2 = 0.5;
 // Position 3:
-const int q3_3 = M_PI/4;
+const double q3_3 = M_PI/4;
 const double cx3 = 0.2;
 const double cy3 = 0.6;
 
 // Set Position (1, 2, or 3)
-const double cx = cx2;
-const double cy = cy2;
-const double q3 = q3_2;
+const double cx = cx1;
+const double cy = cy1;
+const double q3 = q3_1;
 
 // store max torque:
 double min_torque = 100;
@@ -38,10 +38,6 @@ double min_ax = 0;
 double min_ay = 0;
 double min_bx = 0;
 double min_by = 0;
-
-double rad(double deg) {
-    return deg * M_PI / 180;
-}
 
 double get_hypoteneus(double a, double b) {
     return sqrt(a * a + b * b);
@@ -59,6 +55,14 @@ double get_q2(double angle_b, double by, double bx) {
     return atan2(by, bx) - angle_b;
 }
 
+double get_bx(double l3) {
+    return cx - l3 * cos(q3);
+}
+
+double get_by(double l3) {
+    return cy - l3 * sin(q3);
+}
+
 double get_ax(double l1, double q1) {
     return l1 * cos(q1); // note that q1 is in rad
 }
@@ -67,19 +71,11 @@ double get_ay(double l1, double q1) {
     return l1 * sin(q1); // note that q1 is in rad
 }
 
-double get_bx(double l3) {
-    return cx - l3 * cos(rad(q3));
-}
-
-double get_by(double l3) {
-    return cy - l3 * sin(rad(q3));
-}
-
 int main() {
     // iterate for values of l1, l2, and l3
-    for (double l1 = START_LENGTH; l1 <= MAX_LENGTH; l1 += INCREMENT_LENGTH) {
-        for (double l2 = START_LENGTH; l2 <= MAX_LENGTH; l2 += INCREMENT_LENGTH) {
-            for (double l3 = START_LENGTH; l3 <= MAX_LENGTH; l3 += INCREMENT_LENGTH)
+    for (double l1 = MIN_LENGTH; l1 <= MAX_LENGTH; l1 += INCREMENT_LENGTH) {
+        for (double l2 = MIN_LENGTH; l2 <= MAX_LENGTH; l2 += INCREMENT_LENGTH) {
+            for (double l3 = MIN_LENGTH; l3 <= MAX_LENGTH; l3 += INCREMENT_LENGTH)
             {
                 if (l1 + l2 + l3 >= 1) {
                     // determine all unknowns
@@ -92,7 +88,7 @@ int main() {
                     double ay = get_ay(l1, q1);
 
                     // determine moment on origin:
-                    double torque = 9.81*((4*l1)*(0.5*ax) + (2*l2)*(ax+(0.5*(bx-ax))) + (1*l3)*(ax+bx+0.5*(cx-bx)) + 5*cx);
+                    double torque = 9.81*((4*l1)*(0.5*ax) + (2*l2)*(ax+0.5*l2*cos(q2)) + (1*l3)*(cx-0.5*l3*cos(q3)) + 5*cx);
 
                     // print results
                     cout << "l1 = " << l1 << ", l2 = " << l2 << ", l3 = " << l3
